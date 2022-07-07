6ac2ca53779accbc1d48d7e86040ba10cab7049d
kokocan12 <sky203614@gmail.com>
2022-07-07 20:52:04 +0900

    user db 생성, api라우터 파일 생성

--- server/util/session.js
+++ server/util/session.js
@@ -1,43 +1,5 @@
 function getRandomString() {
-  const selection = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-  ];
-
+  const selection = "abcdefghijklmnopqrstuvwxyz0123456789";
   const randomNumber = Math.floor(Math.random() * selection.length);
   return selection[randomNumber];
 }
