5e3d19de78672157696144f7e3f14a7bb383e9f8
이형준 <49009864+kokocan12@users.noreply.github.com>
2022-07-07 16:24:31 +0900

    로그인 페이지 에러메시지 표시 및 회원가입 약관동의 페이지 UI개발 (#13)
    * 로그인 페이지 에러메시지 표시
    로그인 페이지에서 인풋에 값이 없는 경우 에러 메시지 보이도록 처리(#11)
    
    * 회원가입-약관동의 페이지
    회원가입 - 약관동의 페이지 내 체크박스 관련 UI개발 및 조건 일치 시 다음버튼 활성화 되도록 개발

---
+++ public/assets/script/agreement.js
@@ -0,0 +1,85 @@
+function init() {
+  const handler = {
+    get: function (target, key) {
+      return target[key];
+    },
+    set: function (target, key, value) {
+      target[key] = value;
+      if (key === "check-all") {
+        [
+          "usage-agreement",
+          "e-trade-agreement",
+          "personal-info-collect-agreement",
+          "personal-info-share-agreement",
+          "marketing-agreement",
+        ].forEach((v) => {
+          target[v] = target["check-all"];
+        });
+      }
+
+      target["check-all"] = [
+        "usage-agreement",
+        "e-trade-agreement",
+        "personal-info-collect-agreement",
+        "personal-info-share-agreement",
+        "marketing-agreement",
+      ].reduce((prev, v) => {
+        return prev && target[v];
+      }, true);
+
+      notify(target);
+      return true;
+    },
+  };
+
+  const proxy = new Proxy(
+    {
+      "check-all": false,
+      "usage-agreement": false,
+      "e-trade-agreement": false,
+      "personal-info-collect-agreement": false,
+      "personal-info-share-agreement": false,
+      "marketing-agreement": false,
+      "age-over-14": false,
+      "age-under-14": false,
+    },
+    handler
+  );
+
+  const appContainer = document.querySelector(".app-container");
+  appContainer.addEventListener("change", (e) => {
+    if (e.target.tagName === "INPUT") {
+      const key = e.target.id;
+      const value = e.target.checked;
+      proxy[key] = value;
+    }
+  });
+}
+
+function notify(proxy) {
+  [
+    "check-all",
+    "usage-agreement",
+    "e-trade-agreement",
+    "personal-info-collect-agreement",
+    "personal-info-share-agreement",
+    "marketing-agreement",
+  ].forEach((id) => {
+    const el = document.getElementById(id);
+
+    el.checked = proxy[id];
+  });
+
+  const canGoNext = [
+    "usage-agreement",
+    "e-trade-agreement",
+    "personal-info-collect-agreement",
+  ].reduce((prev, value) => {
+    return prev && proxy[value];
+  }, true);
+
+  const nextButtonValidChecker = document.getElementById("valid-check");
+  nextButtonValidChecker.checked = canGoNext;
+}
+
+init();
