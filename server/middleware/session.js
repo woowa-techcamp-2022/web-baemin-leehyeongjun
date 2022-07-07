3cfe3b3649ca69ad1c22937b312882c017248af5
kokocan12 <sky203614@gmail.com>
2022-07-08 01:29:14 +0900

    세션 유지시간 설정

--- server/middleware/session.js
+++ server/middleware/session.js
@@ -1,26 +1,45 @@
 const { makeRandomSessionId } = require("../util/session");
 
 const sessions = new Map();
+const SESSION_LIFETIME = 1000 * 60 * 10;
 
 function setSession(req, res, next) {
   if (!req.cookies["sid"]) {
     const sid = makeRandomSessionId();
-    sessions.set(sid, {});
+    sessions.set(sid, {
+      timer: setTimeout(() => {
+        sessions.delete(sid);
+      }, SESSION_LIFETIME),
+    });
 
     res.cookie("sid", sid, {
       expires: 0,
       httpOnly: true,
     });
   } else {
-    if (!sessions.has(req.cookies["sid"])) {
-      sessions.set(req.cookies["sid"], {});
+    const sid = req.cookies["sid"];
+    if (!sessions.has(sid)) {
+      sessions.set(sid, {
+        timer: setTimeout(() => {
+          sessions.delete(sid);
+        }, SESSION_LIFETIME),
+      });
+    } else {
+      const sid = req.cookies["sid"];
+      const session = sessions.get(sid);
+
+      clearTimeout(session.timer);
+      session.timer = setTimeout(() => {
+        sessions.delete(sid);
+      }, SESSION_LIFETIME);
     }
   }
   next();
 }
 
 function getSession(req) {
-  return sessions.get(req.cookies["sid"]);
+  const sid = req.cookies["sid"];
+  return sessions.get(sid);
 }
 
 module.exports = {
