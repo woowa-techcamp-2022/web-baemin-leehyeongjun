5e5344da9dce600c38917c543b2bd6bbcbbd9afa
kokocan12 <sky203614@gmail.com>
2022-07-07 21:20:11 +0900

    세션 아이디 발급 및 세션 값 가져오기 완료

---
+++ server/middleware/session.js
@@ -0,0 +1,25 @@
+const { makeRandomSessionId } = require("../util/session");
+
+const sessions = new Map();
+
+function setSession(req, res, next) {
+  if (!req.cookies["sid"]) {
+    const sid = makeRandomSessionId();
+    sessions.set(sid, {});
+
+    res.cookie("sid", sid, {
+      expires: 0,
+      httpOnly: true,
+    });
+  }
+  next();
+}
+
+function getSession(req) {
+  return sessions.get(req.cookies["sid"]);
+}
+
+module.exports = {
+  setSession,
+  getSession,
+};
--- server/router/app.js
+++ server/router/app.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const { pug } = require("../util/pug");
 const { ROOT_PATH, join } = require("../util/path");
+const { setSession } = require("../middleware/session");
 const signupRouter = require("./signup");
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.static("public"));
+app.use(setSession);
 
 app.get("/", (req, res) => {
   const mainPath = join(ROOT_PATH, "web", "main.pug");
